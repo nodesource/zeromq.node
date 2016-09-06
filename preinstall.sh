cec0d3cfc381773482982fbf39bde2aceaad3ec7
Thorsten Lorenz <thlorenz@gmx.de>
2016-09-06 17:37:53 +0000

    build: fixing preinstall.sh for older git
    - --single-branch isn't supported by git on our Jenkins

--- preinstall.sh
+++ preinstall.sh
@@ -4,7 +4,7 @@ rm -rf deps/zmq &&                             \
 npm install nan@~2.3 &&                        \
 git clone git@github.com:nodesource/libzmq.git \
   --branch thlorenz/force-static               \
-  --depth 1 --single-branch                    \
+  --depth 1                                    \
   deps/zmq &&                                  \
 rm -rf deps/zmq/.git &&                        \
 node-gyp configure &&                          \
